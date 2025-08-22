db0c22864d6fdd06393b982e6cd800a5d4eeec24
QYikers <bumengeg.288485@fairview.sti.edu.ph>
2025-08-22 15:43:52 +0800

    Added some addtional button for the psr generate modal

---
+++ pages/employee/employeeFunctions/pm-functions.js
@@ -0,0 +1,22 @@
+// This page generates reports for the property management team
+// Add a week button beside the monthly for the Generating weekly reports
+// After clicking a button generate dropdowns ex. when clicking the weekly button it would generate 3 drop downs for
+// 1. Select Week
+// 2. Select Month
+// 3. Select Year
+
+// For the monthly button, it would generate 2 drop downs for
+// 1. Select Month
+// 2. Select Year
+// 
+
+// For Quarterly button, it would generate 2 drop downs for
+// 1. Select Quarter
+// 2. Select Year
+
+// For Semi annual, it would generate 2 drop downs for
+// 1. Select Half
+// 2. Select Year
+
+// For Annual button, it would generate 2 drop downs for
+// 1. Select Year
